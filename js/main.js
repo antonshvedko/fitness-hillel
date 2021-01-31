990658b56be3c16623785605147b7d2d53e13805
antonshvedko <antonshvedko996@gmail.com>
2021-01-31 20:37:30 +0200

    commit

--- js/main.js
+++ js/main.js
@@ -1,6 +1,14 @@
-$(document).ready(function () {
-    $('.menu-opener').on('click', function(e) {
-        e.preventDefault() 
-        $('.header').toggleClass('mobile-menu-active');
-    })
-});
+let menu = document.querySelector('.menu-opener');
+if (typeof menu !== 'undefined') {
+    menu.addEventListener('click', () => {
+        let header = document.querySelector('.header');
+        console.log(menu);
+        if (typeof header !== 'undefined') {
+            if (!header.classList.contains('.mobile-menu-active')) {
+                header.classList.add('mobile-menu-active');
+            } else {
+                header.classList.remove('mobile-menu-active');
+            }
+        }
+    });
+}
